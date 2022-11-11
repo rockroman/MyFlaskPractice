f7ad37543fc5984136efcd5f275b2c3d0d92a3e6
rockroman <rock.rakic@gmail.com>
2022-11-11 15:29:37 +0000

    Add flask flash and display feedback message to user when form is submitted

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -34,8 +35,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form["email"])
-
+        flash("THANKS {}, WE HAVE RECIEVED YOUR MESSAGE!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
